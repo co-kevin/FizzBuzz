86c9b0d2ba7ed8b657895d1247da89ac8a9e6150
Hooks Zhang <hookszhang@gmail.com>
2019-08-11 12:49:06 +0800

    feat: 如果一个数能被3整除，或者包含数字3，那么这个数就是“Fizz”, 如果一个数能被5整除，或者包含数字5，那么这个数就是“Buzz”

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-function say(number) {
-  if (number % 15 === 0) return 'FizzBuzz'
-  if (number % 3 === 0) return 'Fizz'
-  if (number % 5 === 0) return 'Buzz'
+for (let number = 1; number <= 100; number ++) {
+  console.log(say(number))
+}
+
+function say (number) {
+  if (isFizz(number) && isBuzz(number)) return 'FizzBuzz'
+  if (isFizz(number)) return 'Fizz'
+  if (isBuzz(number)) return 'Buzz'
   return number
+}
+
+function isFizz (number) {
+  return number % 3 === 0 || new String(number).includes('3')
+}
+
+function isBuzz (number) {
+  return number % 5 === 0 || new String(number).includes('5')
 }
 
 export { say }
--- test/test.js
+++ test/test.js
@@ -5,13 +5,16 @@ describe('#say()', function () {
   it('should return 1 when give 1', function () {
     assert.equal(say(1), 1)
   })
-  it('should return Fizz when give 3', function () {
+  it('should return Fizz when give 3, 31', function () {
     assert.equal(say(3), 'Fizz')
+    assert.equal(say(31), 'Fizz')
   })
-  it('should return Buzz when give 5', function () {
+  it('should return Buzz when give 5, 52', function () {
     assert.equal(say(5), 'Buzz')
+    assert.equal(say(52), 'Buzz')
   })
-  it('should return FizzBuzz when give 15', function () {
+  it('should return FizzBuzz when give 15, 51', function () {
     assert.equal(say(15), 'FizzBuzz')
+    assert.equal(say(51), 'FizzBuzz')
   })
 })
